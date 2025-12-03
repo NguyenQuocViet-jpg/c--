a8b978ce1cbcf29df7d1b972141f7aee5235fbb9
nguyen quoc viet ps48341 <nguyenviet141125@gmail.com>
2025-12-03 19:07:55 +0700

    Remove exe files from git and uodate gitignore

---
+++ Labs6_bai4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, m;
+    printf("Nhập số hàng trong mảng: ");
+    scanf("%d", &n);
+    printf("Nhập số cột trong mảng: ");
+    scanf("%d", &m);
+
+    int nguyen[n][m];
+    for(int k = 0; k < n; k++)
+    {
+        for(int l = 0; l < m; l++)
+        {
+            printf("Nhập mảng nguyen[%d][%d]: ", k, l);
+            scanf("%d", &nguyen[k][l]);
+        }
+    }
+
+    for(int k = 0; k < n; k++)
+    {
+        for(int l = 0; l < m; l++)
+        {
+            printf("Bình pương phần nguyen[%d][%d] trong mảng: %d\n", k, l, nguyen[k][l] * nguyen[k][l]);
+        }
+    }
+
+    return 0;
+}
